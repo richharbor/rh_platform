b6547b63e7aa27f560efef26ff05f712ab7e0e7c
prabhat <prabhat@rhinontech.com>
2025-12-30 01:40:51 +0530

    chore: Remove old frontend Expo and Next.js applications and shared packages, and update mobile-expo and web-admin configurations.

--- frontend/apps/expo/index.js
+++
@@ -1,7 +0,0 @@
-import 'setimmediate'
-
-if (!global?.setImmediate) {
-  global.setImmediate = setTimeout
-}
-
-import 'expo-router/entry'
--- frontend/packages/ui/src/CustomToast.tsx
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native'
-import { NativeToast as Toast } from './NativeToast'
-
-export const CustomToast = () => {
-  if (Platform.OS === 'web') return null
-  return <Toast />
-}
